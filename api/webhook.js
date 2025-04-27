65909922b714bde893b6ba5f71f2fd41fd0b9b8d
viktorpisemnet <viktor.pisem.net@gmail.com>
2025-04-27 14:58:47 -0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,59 +2,34 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     console.log('Webhook received:', req.body);
 
-    const {
-      Name,
-      Email,
-      Phone,
-      Street_Address,
-      City,
-      State,
-      ZIP_Code
-    } = req.body;
-
     const shippoToken = process.env.SHIPPO_API_KEY;
 
-    const orderData = {
-      address_to: {
-        name: Name,
-        street1: Street_Address,
-        city: City,
-        state: State,
-        zip: ZIP_Code,
-        country: 'US',
-        email: Email,
-        phone: Phone,
-      },
-      parcels: [
-        {
-          length: '10',
-          width: '10',
-          height: '10',
-          distance_unit: 'in',
-          weight: '1',
-          mass_unit: 'lb'
-        }
-      ],
-      async: false
-    };
-
     try {
-      const response = await fetch('https://api.goshippo.com/shipments/', {
+      const response = await fetch('https://api.goshippo.com/addresses/', {
         method: 'POST',
         headers: {
           'Authorization': `ShippoToken ${shippoToken}`,
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(orderData)
+        body: JSON.stringify({
+          name: "John Doe",
+          street1: "6512 Greene Rd.",
+          city: "Woodridge",
+          state: "IL",
+          zip: "60517",
+          country: "US",
+          phone: "+1 555 341 9393",
+          email: "johndoe@example.com"
+        })
       });
 
       const data = await response.json();
-      console.log('Shippo order created:', data);
+      console.log('Shippo address created:', data);
 
-      res.status(200).json({ message: 'Order created successfully', data });
+      res.status(200).json({ message: 'Address created successfully', data });
     } catch (error) {
-      console.error('Error creating order:', error);
-      res.status(500).json({ error: 'Failed to create order' });
+      console.error('Error creating address:', error);
+      res.status(500).json({ error: 'Failed to create address' });
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
